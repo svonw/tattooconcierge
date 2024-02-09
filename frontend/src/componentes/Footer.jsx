b0d0f73ea79ecf33920b63f6e2565a5624d7cae4
svonw <88006181+svonw@users.noreply.github.com>
2024-02-09 11:20:06 +0100

    footer actualizado

--- frontend/src/componentes/Footer.jsx
+++ frontend/src/componentes/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="py-5 mt-2 bg-zinc-300">
-      <p className="text-center text-zinc-600 font-Cinzel font-semibold">
+      <p className="text-center text-zinc-700 font-Cinzel font-semibold">
         TATTOO
         <span className="text-black"> CONCIERGE</span>
       </p>
